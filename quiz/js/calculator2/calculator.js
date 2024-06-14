2a3741d9a9427e98776f225b88a6aad481f65359
YoonSeokChan98 <shift71895@gmail.com>
2024-06-15 04:53:56 +0900

    calculator1

---
+++ quiz/js/calculator2/calculator.js
@@ -0,0 +1,46 @@
+let sumNum = '';
+let oper = '';
+let prevNum = '';
+
+let clearCalc = function(){
+    sumNum = '';
+    oper = '';
+    prevNum = '';
+}
+
+let number = function(num){
+    sumNum += num;
+    console.log("입력숫자: " + sumNum);
+}
+
+let sign = function(sin){
+    oper = sin;
+    prevNum = sumNum;
+    sumNum = '';
+    console.log("입력문자: " + sin);
+}
+
+let calculate = function(){
+    let num1 = Number(prevNum);
+    let num2 = Number(sumNum);
+    let result;
+    switch(oper){
+        case "*":
+            result = num1 * num2;
+            break;
+        case "/":
+            result = num1 / num2;
+            break;
+        case "+":
+            result = num1 + num2;
+            break;
+        case "-":
+            result = num1 - num2;
+            break;
+        default:
+            result = "Error";
+    }
+    alert("결과는: " + result);
+    clearCalc(); // 계산 후 초기화
+}
+clearCalc()
